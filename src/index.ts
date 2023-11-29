efe484b998c256aa558c0f4ad11b3deb2894e446
givigelashvili <55489231+aquarazorda@users.noreply.github.com>
2023-11-30 02:30:53 +0400

    Initial version

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,78 @@
-console.log(process.env.PORT)
+import http from "http";
+import fetch from "node-fetch";
+import "dotenv/config";
+
+const { MS_MAIN_URL, MS_LOGIN_URL, MS_LOGIN_EMAIL, MS_LOGIN_PASSWORD, PORT } =
+  process.env;
+
+if (
+  !MS_MAIN_URL ||
+  !MS_LOGIN_URL ||
+  !MS_LOGIN_EMAIL ||
+  !MS_LOGIN_PASSWORD ||
+  !PORT
+) {
+  throw new Error("Missing environment variables");
+}
+
+const reusableHeaders = {
+  host: MS_MAIN_URL.replace("https://", ""),
+  "user-agent":
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15",
+  "cache-control": "no-cache, no-store",
+  accept: "*/*",
+  "accept-encoding": "gzip, deflate, br",
+};
+
+const login = async () => {
+  let urlencoded = new URLSearchParams();
+  urlencoded.append("action", "login");
+  urlencoded.append("login", MS_LOGIN_EMAIL);
+  urlencoded.append("password", MS_LOGIN_PASSWORD);
+
+  const response = await fetch(MS_MAIN_URL + MS_LOGIN_URL, {
+    headers: {
+      ...reusableHeaders,
+      "content-type": "application/x-www-form-urlencoded",
+    },
+    body: urlencoded,
+    method: "POST",
+  });
+
+  const cookies = response.headers.get("set-cookie")?.split(";");
+  const pid = cookies?.[0];
+  const cid = cookies?.[1].split(", ")[1];
+  const newCookies = pid + "; " + cid;
+
+  console.log(await response.text());
+
+  return newCookies;
+};
+
+const server = http.createServer(async (req, res) => {
+  const cookie = await login();
+
+  let body = "";
+  req
+    .on("data", (data) => {
+      body += data;
+      if (body.length > 1e6) req.connection.destroy();
+    })
+    .on("end", async () => {
+      const response = await fetch(MS_MAIN_URL + req.url, {
+        headers: {
+          ...reusableHeaders,
+          cookie,
+        },
+        method: req.method,
+        body: req.method !== "GET" && req.method !== "HEAD" ? body : undefined,
+      });
+
+      res.statusCode = response.status;
+      res.end(await response.text());
+    });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
